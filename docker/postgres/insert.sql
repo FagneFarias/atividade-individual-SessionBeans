a7eaa9627d7045088981a19edde327b281b30b19
FagneFarias <fagner-rolim@hotmail.com>
2022-03-17 21:56:47 -0300

    adicionando estrutura e banco

---
+++ docker/postgres/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Jennifer','44444444444');
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Camila','55555555555');
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Fagne','88888888888');
+
+
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Mouse Multilaser','300');
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Macbook','5500');
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Headset Hypex','1540');
+
+
+INSERT INTO VENDA (produto, cliente) VALUES (1, 1);
+INSERT INTO VENDA (produto, cliente) VALUES (2, 2);
+INSERT INTO VENDA (produto, cliente) VALUES (3, 3);
